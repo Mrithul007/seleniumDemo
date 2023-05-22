719a4b165a9c7639e7e952092cb9c13d4078fe65
Mrithul <msdharma007@gmail.com>
2023-05-22 15:24:24 +0530

    program

---
+++ src/pack/Print.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 25;
+		int b = 25;
+		int Sum = a + b;
+		System.out.println("Sum = " + Sum);
+	}
+
+}
